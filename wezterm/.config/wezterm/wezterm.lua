449c2bcea34ab611cb30c00960f72a3168f5bf4b
Michael Murphy <github@elmurphy.com>
2025-12-01 12:12:45 +1100

    feat(wezterm): adding inital configuration

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,130 @@
+local wezterm = require("wezterm")
+local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
+local config = wezterm.config_builder()
+
+-- Show which key table is active in the status area
+wezterm.on("update-right-status", function(window, pane)
+	local name = window:active_key_table()
+	if name then
+		name = "TABLE: " .. name
+	end
+	window:set_right_status(name or "")
+end)
+
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
+config.keys = {
+	{ key = "h", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Right") },
+	{ key = "c", mods = "LEADER", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+	{ key = "x", mods = "LEADER", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
+	{ key = "p", mods = "LEADER", action = wezterm.action.ActivateTabRelative(-1) },
+	{ key = "n", mods = "LEADER", action = wezterm.action.ActivateTabRelative(1) },
+	{ key = "|", mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "-", mods = "LEADER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "z", mods = "LEADER", action = wezterm.action.TogglePaneZoomState },
+	{ key = "[", mods = "LEADER", action = wezterm.action.ActivateCopyMode },
+	{ key = " ", mods = "LEADER", action = wezterm.action.QuickSelect },
+	{ key = "w", mods = "LEADER", action = wezterm.action.ShowTabNavigator },
+	{
+		key = "L",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.Multiple({
+			wezterm.action.ClearScrollback("ScrollbackAndViewport"),
+			wezterm.action.SendKey({ key = "L", mods = "CTRL" }),
+		}),
+	},
+	{
+		key = ",",
+		mods = "LEADER",
+		action = wezterm.action.PromptInputLine({
+			description = "Enter name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+	{
+		key = "r",
+		mods = "LEADER",
+		action = wezterm.action.ActivateKeyTable({
+			name = "resize_pane",
+			one_shot = false,
+		}),
+	},
+}
+
+config.key_tables = {
+	-- Defines the keys that are active in our resize-pane mode.
+	resize_pane = {
+		{ key = "h", action = wezterm.action.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "l", action = wezterm.action.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "k", action = wezterm.action.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "j", action = wezterm.action.AdjustPaneSize({ "Down", 1 }) },
+		-- Cancel the mode by pressing escape
+		{ key = "Escape", action = "PopKeyTable" },
+	},
+}
+
+-- Leader + number to switch tab
+for i = 1, 8 do
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = wezterm.action.ActivateTab(i - 1),
+	})
+end
+
+-- Tab bar
+config.hide_tab_bar_if_only_one_tab = false
+config.use_fancy_tab_bar = false
+config.switch_to_last_active_tab_when_closing_tab = true
+
+-- General
+config.colors = {
+  foreground = "#f7f1ff",
+  background = "#222222",
+  cursor_bg = "#bab6c0",
+  cursor_fg = "#bab6c0",
+  cursor_border = "#bab6c0",
+  selection_fg = "#f7f1ff",
+  selection_bg = "#525053",
+  ansi = {
+    "#222222",
+    "#fc618d",
+    "#7bd88f",
+    "#fce566",
+    "#fd9353",
+    "#948ae3",
+    "#5ad4e6",
+    "#f7f1ff",
+  },
+  brights = {
+    "#69676c",
+    "#fc618d",
+    "#7bd88f",
+    "#fce566",
+    "#fd9353",
+    "#948ae3",
+    "#5ad4e6",
+    "#f7f1ff",
+  },
+}
+config.font = wezterm.font("TX-02")
+config.font_size = 11.0
+config.use_dead_keys = false
+config.window_decorations = "RESIZE"
+config.adjust_window_size_when_changing_font_size = false
+config.window_close_confirmation = "NeverPrompt"
+
+smart_splits.apply_to_config(config, {
+	direction_keys = { "h", "j", "k", "l" },
+	modifiers = {
+		move = "CTRL",
+	},
+})
+
+return config
