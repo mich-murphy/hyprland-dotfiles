708654666b627c73f70c28531109fb3c9d26acdc
Michael Murphy <github@elmurphy.com>
2025-12-08 12:26:20 +1100

    fix(scripts): setup initial test structure and base for theme_switcher.py

--- scripts/.local/bin/theme_switcher.py
+++ scripts/.local/bin/theme_switcher.py
@@ -2,9 +2,10 @@
 
 import argparse
 import os
+import subprocess
+import tempfile
 from enum import Enum
 from pathlib import Path
-from typing import Optional
 
 import pytest
 
@@ -19,48 +20,55 @@ parser = argparse.ArgumentParser(
     description="Manages switching of user specified system themes",
 )
 parser.add_argument("theme", choices=[theme.value for theme in Themes])
+parser.add_argument("-t", "--theme-folder", default="colorschemes")
+parser.add_argument("-c", "--config-path", default=os.environ["XDG_CONFIG_HOME"])
+parser.add_argument("-d", "--data-path", default=os.environ["XDG_DATA_HOME"])
 args = parser.parse_args()
 theme = args.theme
+theme_folder = args.theme_folder
+config_path = args.config_path
+data_path = args.data_path
 working_directory = os.getcwd()
 
 
-def validate_dirs(
-    theme: str = theme,
-    theme_dir: str = "colorschemes",
-    config_dir: str = os.environ["XDG_CONFIG_HOME"],
-) -> Optional[Path]:
-    if not os.path.exists(config_dir):
-        raise ValueError(f"Config directory does not exist at: {config_dir}")
-    theme_dir = os.path.join(config_dir, theme_dir, theme)
-    if not os.path.exists(theme_dir):
-        raise ValueError(
-            f"Colorscheme directory not found at: {config_dir}/colorschemes/{theme}"
-        )
-    return Path(theme_dir)
+def validate_path_exists(path: str) -> Path:
+    """Confirm user provided paths exist on file system."""
+    if not os.path.exists(path):
+        raise ValueError(f"Directory does not exist at: {path}")
+    return Path(path)
+
+
+CONFIG_PATH = validate_path_exists(config_path)
+DATA_PATH = validate_path_exists(data_path)
+THEME_PATH = validate_path_exists(f"{CONFIG_PATH}/{theme_folder}/{theme}")
+
+
+def apply_rofi_theme(
+    rofi_source: str = f"{THEME_PATH}/rofi/theme.rasi",
+    rofi_target: str = f"{DATA_PATH}/rofi/theme.rasi",
+):
+    """Copy rofi config to set specified theme."""
+    print("Applying Rofi theme...\n")
+    valid_rofi_source = validate_path_exists(rofi_source)
+    command = subprocess.run(["cp", valid_rofi_source, rofi_target])
+    return command.returncode
 
 
 if __name__ == "__main__":
 
-    def test_validate_dirs(theme: str = theme):
-        expected = Path(f"/home/mm/.config/colorschemes/{theme}")
-        result = validate_dirs(theme)
-        assert result == expected
-
-    @pytest.mark.parametrize(
-        theme_dir, config_dir,
-        [
-            ("fake-theme-dir", os.environ["ZDG_CONFIG_HOME"]),
-            ("colorschemes", "fake-config-dir"),
-        ],
-    )
-    def test_validate_dirs_invalid_config_dir(theme: str = theme, theme_dir, config_dir):
+    def test_validate_theme_path_invalid_folder():
+        theme_folder = "fake-theme-dir"
         with pytest.raises(ValueError):
-            validate_dirs(theme, config_dir="fake-config-dir")
+            validate_path_exists(theme_folder)
 
-    def test_validate_dirs_invalid_theme_dir(theme: str = theme):
-        with pytest.raises(ValueError):
-            validate_dirs(theme, theme_dir="fake-theme-dir")
+    def test_apply_rofi_theme():
+        with tempfile.NamedTemporaryFile() as temp_rofi_source:
+            with tempfile.TemporaryDirectory() as temp_rofi_target_dir:
+                result = apply_rofi_theme(
+                    f"{temp_rofi_source.name}",
+                    f"{temp_rofi_target_dir}/{os.path.basename(temp_rofi_source.name)}",
+                )
+                assert result == 0
 
-    test_validate_dirs()
-    test_validate_dirs_invalid_config_dir()
-    test_validate_dirs_invalid_theme_dir()
+    test_validate_theme_path_invalid_folder()
+    test_apply_rofi_theme()
