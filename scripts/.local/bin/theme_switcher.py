e47b6b4d8fa5046ed4d6aa93cc832c04c67f1780
Michael Murphy <github@elmurphy.com>
2025-12-12 15:21:28 +1100

    fix(scripts): adding working theme_switcher.py

--- scripts/.local/bin/theme_switcher.py
+++ scripts/.local/bin/theme_switcher.py
@@ -4,7 +4,6 @@ import argparse
 import os
 import shutil
 import subprocess
-import unittest
 from enum import Enum
 from pathlib import Path
 
@@ -20,10 +19,6 @@ class Theme(Enum):
         "general": "carbonfox",
         "gtk": "Nightfox-Dark",
     }
-
-    @classmethod
-    def instantiate(cls, theme):
-        return cls(theme)
 
 
 parser = argparse.ArgumentParser(
@@ -56,7 +51,7 @@ class Application(Enum):
 
     hyprland = {
         "name": "hypr",
-        "source": f"{THEME_PATH}/hypr/colorscheme.rasi",
+        "source": f"{THEME_PATH}/hypr/colorscheme.conf",
         "target": f"{DOTFILES_PATH}/hypr/.config/hypr/colorscheme.conf",
         "stow": True,
     }
@@ -76,6 +71,12 @@ class Application(Enum):
         "name": "wezterm",
         "source": f"{THEME_PATH}/wezterm/colorscheme.lua",
         "target": f"{DOTFILES_PATH}/wezterm/.config/wezterm/colorscheme.lua",
+        "stow": True,
+    }
+    ghostty = {
+        "name": "wezterm",
+        "source": f"{THEME_PATH}/ghostty/colorscheme",
+        "target": f"{DOTFILES_PATH}/ghostty/.config/ghostty/colorscheme",
         "stow": True,
     }
     zathura = {
@@ -104,109 +105,86 @@ class Application(Enum):
     }
 
 
-def apply_theme_file(application: Application) -> int:
+def apply_theme_file(application: Application) -> None:
     """Copy application files to set the specified theme from argparse command."""
     source = validate_path(application.value["source"])
     target = application.value["target"]
     stow_required = application.value["stow"]
-    print(f"Copying {application.name.title()} theme file...\n")
     shutil.copyfile(source, target)
     if stow_required:
-        os.chdir(DOTFILES_PATH)
-        stow = subprocess.run(["stow", application.value["name"]])
-        if stow.returncode != 0:
-            return stow.returncode
-    return 0
+        subprocess.run(
+            ["stow", application.value["name"]], cwd=DOTFILES_PATH, check=True
+        )
 
 
-def apply_gtk_theme(theme: Theme) -> int:
+def apply_theme_gtk(theme: Theme) -> None:
     """Set matching GTK theme for specified theme."""
-    print(f"Setting GTK theme to {theme.value['gtk']}...")
-    gsettings = subprocess.run(
+    subprocess.run(
         [
             "gsettings",
             "set",
             "org.gnome.desktop.interface",
             "gtk-theme",
             theme.value["gtk"],
-        ]
+        ],
+        check=True,
     )
-    if gsettings.returncode != 0:
-        return gsettings.returncode
     source = validate_path(f"{THEME_PATH}/gtk-4.0")
     target = f"{CONFIG_PATH}/gtk-4.0"
-    print(f"Linking GTK theme files for {theme.value['gtk']}...")
     os.makedirs(target, exist_ok=True)
-    os.symlink(f"{source}/gtk.css", f"{target}/gtk.css")
-    os.symlink(f"{source}/gtk-dark.css", f"{target}/gtk-dark.css")
-    os.symlink(f"{source}/assets", f"{target}/assets", target_is_directory=True)
-    return 0
+    subprocess.run(["ln", "-sf", f"{source}/gtk.css", f"{target}/gtk.css"], check=True)
+    subprocess.run(
+        ["ln", "-sf", f"{source}/gtk-dark.css", f"{target}/gtk-dark.css"], check=True
+    )
+    subprocess.run(["ln", "-sfn", f"{source}/assets", f"{target}/assets"], check=True)
 
 
-def reload_applications() -> int:
+def reload_applications() -> None:
     """Reload applications to apply the new theme files."""
-    print("Reloading Zsh config...\n")
-    source_zsh = subprocess.run(["source", f"{CONFIG_PATH}/zsh/.zshrc"])
-    if source_zsh != 0:
-        return source_zsh.returncode
-    print("Restarting Waybar and SwayNC...\n")
-    restart_waybar = subprocess.run(["killall", "-9", "waybar", "&&", "waybar", "&"])
-    restart_swaync = subprocess.run(["killall", "-9", "swaync", "&&", "swaync", "&"])
-    if restart_swaync.returncode != 0 or restart_waybar.returncode != 0:
-        return restart_waybar.returncode + restart_swaync.returncode
-    return 0
+    print("Restarting applications...")
+    subprocess.run(
+        ["killall", "-9", "waybar"],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+    subprocess.Popen("waybar", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(
+        ["killall", "-9", "swaync"],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+    subprocess.Popen("swaync", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    # pgrep_wezterm = subprocess.run(
+    #     ["pgrep", "wezterm"],
+    #     capture_output=True,
+    # )
+    # subprocess.run(["xargs", "-r", "kill", "-SIGUSR1"], input=pgrep_wezterm.stdout)
+    # pgrep_ghostty = subprocess.run(
+    #     ["pgrep", "ghostty"],
+    #     capture_output=True,
+    # )
+    # subprocess.run(["xargs", "-r", "kill", "-SIGUSR1"], input=pgrep_ghostty.stdout)
 
 
 def main():
     try:
-        success = []
-        failure = []
         # Apply vicinae theme - can be set via command line
-        vicinae = subprocess.run(["vicinae", "theme", "set", THEME])
-        if vicinae.returncode == 0:
-            success.append("vicinae")
-        else:
-            failure.append("vicinae")
+        subprocess.run(["vicinae", "theme", "set", THEME], check=True)
         # Apply all remaining applicaiton themes following same pattern
         for application in Application:
-            result = apply_theme_file(application)
-            if result == 0:
-                success.append(application.name)
-            else:
-                failure.append(application.name)
+            apply_theme_file(application)
         # Apply GTK theme - follows it's own steps
-        gtk_theme = Theme.instantiate(THEME)
-        if apply_gtk_theme(gtk_theme) == 0:
-            success.append("gtk")
-        else:
-            failure.append("gtk")
+        gtk_theme = Theme[THEME.replace("-", "_")]
+        apply_theme_gtk(gtk_theme)
         # Handle reporting of status
-        if len(success) > 0:
-            print("The following applications had theme successfully applied: \n")
-            for app in success:
-                print(f"{app} \n")
-        if len(failure) > 0:
-            print(
-                "The following applications had errors during their theme being applied: \n"
-            )
-            for app in failure:
-                print(f"{app} \n")
+        print(f"{'*' * 20}")
+        print(f"Theme has been successfully switched to: {THEME}")
+        print(f"{'*' * 20}")
         # Handle reloading of applications all at the same time
-        reload = reload_applications()
-        if reload != 0:
-            print(f"Reloading applications failed with exit code: {reload}")
+        reload_applications()
     finally:
         os.chdir(WORKING_DIRECTORY)
 
 
 if __name__ == "__main__":
-
-    class TestThemeSwitcher(unittest.TestCase):
-        def test_validate_path(self):
-            expected = Path("/tmp")
-            result = validate_path("/tmp")
-            self.assertEqual(expected, result)
-            with self.assertRaises(ValueError):
-                validate_path("fake-theme-dir")
-
-    unittest.main()
+    main()
