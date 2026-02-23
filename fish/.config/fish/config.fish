d047c181b0c9c5c3a8423396f8a04cf75506ec71
Michael Murphy <github@elmurphy.com>
2026-02-23 15:52:27 +1100

    fix: adding mako config and new monitors to niri

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,13 +1,14 @@
-alias ls="eza -la --icons --group-directories-first"
-
 set -g fish_key_bindings fish_vi_key_bindings
 set -g fish_greeting
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+if status is-login
+  set -x BROWSER chromium
 end
 
-starship init fish | source
-direnv hook fish | source
-zoxide init fish | source
-fzf --fish | source
+if status is-interactive
+    alias ls="eza -la --icons --group-directories-first"
+    starship init fish | source
+    direnv hook fish | source
+    zoxide init fish | source
+    fzf --fish | source
+end
